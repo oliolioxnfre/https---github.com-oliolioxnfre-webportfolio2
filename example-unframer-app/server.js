4ee045bdf72a5b98c3a152cdf3642ae41ea1b6fc
oliverking <themedialions@gmail.com>
2026-04-08 02:31:23 -0400

    zzz

--- example-unframer-app/server.js
+++ example-unframer-app/server.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'dist')));
